24d948da225e6319a7597383e381c4a01c6cc3a1
hudson8811 <hudson88@yandex.ru>
2021-01-22 19:10:46 +0300

    chanegd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,22 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    $('.index_banner-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              arrows: true
+            }
+          }
+        ]
+      });
+});
